719db5b56c9ba26a97ecc179b324ade738231b1b
Treata11 <treata11@yahoo.com>
2024-10-04 12:01:35 +0330

    Added `Perspective` projection method; Clean-ups and minor fixes.

--- matrix.h
+++ matrix.h
@@ -88,11 +88,7 @@ simd_half4x4 SIMD_CFUNC simd_translate(simd_half3 v);
 simd_float4x4 SIMD_CFUNC simd_translate(simd_float3 v);
 simd_double4x4 SIMD_CFUNC simd_translate(simd_double3 v);
 
-// TODO: float4x4_translate_in_place
-
 // TODO: float4x4_from_vec3_mul_outer
-
-// TODO: float4x4_rotate
 
 simd_half4x4 SIMD_CFUNC simd_xRotate(simd_half1 angleRadians);
 simd_float4x4 SIMD_CFUNC simd_xRotate(float angleRadians);
@@ -102,17 +98,19 @@ simd_half4x4 SIMD_CFUNC simd_yRotate(simd_half1 angleRadians);
 simd_float4x4 SIMD_CFUNC simd_yRotate(float angleRadians);
 simd_double4x4 SIMD_CFUNC simd_yRotate(double angleRadians);
 
-simd_half4x4 SIMD_CFUNC simd_zRotate(simd_half1 angleRadians);
+simd_half4x4 SIMD_CFUNC simd_zRotate(simd_half1 tangleRadians);
 simd_float4x4 SIMD_CFUNC simd_zRotate(float angleRadians);
 simd_double4x4 SIMD_CFUNC simd_zRotate(double angleRadians);
 
-void SIMD_CFUNC simd_float4x4_orthonormalize(simd_float4x4 R, simd_float4x4 const M);
+simd_float4x4 SIMD_CFUNC simd_float4x4_orthonormalize(simd_float4x4 M);
 
 // TODO: float4x4_frustum
 
 // TODO: float4x4_ortho
 
-// TODO: float4x4_perspective
+simd_half4x4 SIMD_CFUNC simd_perspective(simd_half1 fovYRadians, simd_half1 aspectRatio, simd_half1 nearPlane, simd_half1 farPlane);
+simd_float4x4 SIMD_CFUNC simd_perspective(float fovYRadians, float aspectRatio, float nearPlane, float farPlane);
+simd_double4x4 SIMD_CFUNC simd_perspective(double fovYRadians, double aspectRatio, double nearPlane, double farPlane);
 
 // TODO: float4x4_look_at
 
@@ -145,21 +143,31 @@ namespace simd {
     SIMD_CPPFUNC float4x4 translate(const float3 &v) { return ::simd_translate(v); }
     SIMD_CPPFUNC double4x4 translate(const double3 &v) { return ::simd_translate(v); }
 
-    // SIMD_CPPFUNC float4x4 rotate(float4x4 M, const float3 v, float angle) { return ::simd_rotate(M, v, angle); }
-
-    SIMD_CPPFUNC half4x4 xRotate(simd_half1 angleRadians) { return ::simd_xRotate(angleRadians); }
+    SIMD_CPPFUNC half4x4 xRotate(half1 angleRadians) { return ::simd_xRotate(angleRadians); }
     SIMD_CPPFUNC float4x4 xRotate(float angleRadians) { return ::simd_xRotate(angleRadians); }
     SIMD_CPPFUNC double4x4 xRotate(double angleRadians) { return ::simd_xRotate(angleRadians); }
 
-    SIMD_CPPFUNC half4x4 yRotate(simd_half1 angleRadians) { return ::simd_yRotate(angleRadians); }
+    SIMD_CPPFUNC half4x4 yRotate(half1 angleRadians) { return ::simd_yRotate(angleRadians); }
     SIMD_CPPFUNC float4x4 yRotate(float angleRadians) { return ::simd_yRotate(angleRadians); }
     SIMD_CPPFUNC double4x4 yRotate(double angleRadians) { return ::simd_yRotate(angleRadians); }
 
-    SIMD_CPPFUNC half4x4 zRotate(simd_half1 angleRadians) { return ::simd_zRotate(angleRadians); }
+    SIMD_CPPFUNC half4x4 zRotate(half1 angleRadians) { return ::simd_zRotate(angleRadians); }
     SIMD_CPPFUNC float4x4 zRotate(float angleRadians) { return ::simd_zRotate(angleRadians); }
     SIMD_CPPFUNC double4x4 zRotate(double angleRadians) { return ::simd_zRotate(angleRadians); }
 
-    SIMD_CPPFUNC void simd_float4x4_orthonormalize(simd_float4x4 R, simd_float4x4 const M) { return ::simd_float4x4_orthonormalize(R, M); };
+//    SIMD_CPPFUNC simd_float4x4 simd_float4x4_orthonormalize(simd_float4x4 R, simd_float4x4 const M) { return ::simd_float4x4_orthonormalize(R, M); };
+
+
+
+    SIMD_CPPFUNC half4x4 perspective(half1 fovYRadians, half1 aspectRatio, half1 nearPlane, half1 farPlane) {
+        return ::simd_perspective(fovYRadians, aspectRatio, nearPlane, farPlane);
+    }
+    SIMD_CPPFUNC float4x4 perspective(float fovYRadians, float aspectRatio, float nearPlane, float farPlane) {
+        return ::simd_perspective(fovYRadians, aspectRatio, nearPlane, farPlane);
+    }
+    SIMD_CPPFUNC double4x4 perspective(double fovYRadians, double aspectRatio, double nearPlane, double farPlane) {
+        return ::simd_perspective(fovYRadians, aspectRatio, nearPlane, farPlane);
+    }
 
 }
 
@@ -205,8 +213,6 @@ simd_double4x4 SIMD_CFUNC simd_translate(simd_double3 v)
     const simd_double4 col3 = { v.x,   v.y,  v.z, 1.0f };
     return simd_matrix(col0, col1, col2, col3);
 }
-
-
 
 simd_half4x4 SIMD_CFUNC simd_xRotate(simd_half1 angleRadians)
 {
@@ -293,9 +299,57 @@ simd_double4x4 SIMD_CFUNC simd_zRotate(double angleRadians)
 }
 
 // TODO: Implement
-void SIMD_CFUNC simd_float4x4_orthonormalize(simd_float4x4 R, simd_float4x4 const M)
+/// [Gram-Schmidt](https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process)
+simd_float4x4 SIMD_CFUNC simd_float4x4_orthonormalize(simd_float4x4 M)
 {
 
+    return matrix_identity_float4x4;
+}
+
+
+
+// TODO: Tests
+simd_half4x4 SIMD_CFUNC simd_perspective(simd_half1 fovYRadians, simd_half1 aspectRatio, simd_half1 nearPlane, simd_half1 farPlane)
+{
+  // Calculate the tangent of half the vertical field of view
+    const simd_half1 tanHalfFovY = tanf(fovYRadians * 0.5f);
+    
+    const simd_half1 nearPlaneWidth = nearPlane * tanHalfFovY * aspectRatio;
+    const simd_half1 nearPlaneHeight = nearPlane * tanHalfFovY;
+
+    const simd_half4 col0 = { nearPlaneWidth, 0.0f, 0.0f, 0.0f };
+    const simd_half4 col1 = { 0.0f, nearPlaneHeight, 0.0f, 0.0f };
+    const simd_half4 col2 = { 0.0f, 0.0f, (farPlane + nearPlane) / (nearPlane - farPlane), (2 * farPlane * nearPlane) / (nearPlane - farPlane) };
+    const simd_half4 col3 = { 0.0f, 0.0f, -1.0f, 0.0f };
+    return simd_matrix(col0, col1, col2, col3);
+}
+simd_float4x4 SIMD_CFUNC simd_perspective(float fovYRadians, float aspectRatio, float nearPlane, float farPlane)
+{
+  // Calculate the tangent of half the vertical field of view
+    const float tanHalfFovY = tanf(fovYRadians * 0.5f);
+    
+    const float nearPlaneWidth = nearPlane * tanHalfFovY * aspectRatio;
+    const float nearPlaneHeight = nearPlane * tanHalfFovY;
+
+    const simd_float4 col0 = { nearPlaneWidth, 0.0f, 0.0f, 0.0f };
+    const simd_float4 col1 = { 0.0f, nearPlaneHeight, 0.0f, 0.0f };
+    const simd_float4 col2 = { 0.0f, 0.0f, (farPlane + nearPlane) / (nearPlane - farPlane), (2.0f * farPlane * nearPlane) / (nearPlane - farPlane) };
+    const simd_float4 col3 = { 0.0f, 0.0f, -1.0f, 0.0f };
+    return simd_matrix(col0, col1, col2, col3);
+}
+simd_double4x4 SIMD_CFUNC simd_perspective(double fovYRadians, double aspectRatio, double nearPlane, double farPlane)
+{
+  // Calculate the tangent of half the vertical field of view
+    const double tanHalfFovY = tanf(fovYRadians * 0.5f);
+    
+    const double nearPlaneWidth = nearPlane * tanHalfFovY * aspectRatio;
+    const double nearPlaneHeight = nearPlane * tanHalfFovY;
+
+    const simd_double4 col0 = { nearPlaneWidth, 0.0f, 0.0f, 0.0f };
+    const simd_double4 col1 = { 0.0f, nearPlaneHeight, 0.0f, 0.0f };
+    const simd_double4 col2 = { 0.0f, 0.0f, (farPlane + nearPlane) / (nearPlane - farPlane), (2.0f * farPlane * nearPlane) / (nearPlane - farPlane) };
+    const simd_double4 col3 = { 0.0f, 0.0f, -1.0f, 0.0f };
+    return simd_matrix(col0, col1, col2, col3);
 }
 
 #ifdef __cplusplus
